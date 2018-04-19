43b5d524ff3e626e7e4f3c41a8dcc93c8af78e66
Martin Wilkins <martin.wilkins@answerdigital.com>
2018-04-19 09:40:47 +0100

    update scenario context switch to be automatic

--- Demonstrator/Demonstrator.WebApp/AppSrc/core/helpers/modals/scenario-modal.ts
+++ Demonstrator/Demonstrator.WebApp/AppSrc/core/helpers/modals/scenario-modal.ts
@@ -2,11 +2,58 @@
 
 export class ScenarioModalCustomElement {
 
-    @bindable({ defaultBindingMode: bindingMode.oneWay }) cmodule: string;
-    @bindable({ defaultBindingMode: bindingMode.oneWay }) title: string;
+    @bindable({ defaultBindingMode: bindingMode.oneWay })
+    cmodule: string;
+
+    @bindable({ defaultBindingMode: bindingMode.oneWay })
+    title: string;
+
+    switchContext: boolean = false;
+
+    switchTimer: any;
+
+    switchAnimation: boolean = true;
+
+    toggleContext() {
+        this.switchContext = !this.switchContext;
+    }
+
+    toggleAnimation() {
+        this.switchAnimation = !this.switchAnimation;
+
+        if (!this.switchAnimation) {
+            this.stopContext();
+        }
+        else {
+            this.animateContext();
+        }
+    }
+
+    animateContext() {
+        this.switchTimer = window.setInterval(() => this.toggleContext(), 2500);
+    }
+
+    initContext() {
+        let ani = window.setTimeout(() => {
+            this.toggleContext();
+            this.animateContext();
+        }, 1000);
+    }
+
+    stopContext() {
+        window.clearInterval(this.switchTimer);
+    }
 
     loadScenarioModal() {
 
         $('.nrls-scenario-modal').modal('show');
+
+        $('.nrls-scenario-modal').on('shown.bs.modal', () => {
+            this.initContext();
+        });
+
+        $('.nrls-scenario-modal').on('hide.bs.modal', () => {
+            this.stopContext();
+        })
     }
 }
