0c187b8e4feda80ba8b602f401e437e310cbab36
Martin Wilkins <martin.wilkins@answerdigital.com>
2018-04-24 09:55:58 +0100

    additional enum checks

--- Demonstrator/DemonstratorTest.Utilities/EnumHelperTests.cs
+++ Demonstrator/DemonstratorTest.Utilities/EnumHelperTests.cs
@@ -1,4 +1,5 @@
 using Demonstrator.Utilities;
+using Demonstrator.Utilities.Exceptions;
 using Xunit;
 
 namespace DemonstratorTest.Utilities
@@ -6,23 +7,29 @@ namespace DemonstratorTest.Utilities
     public class EnumHelperTests
     {
         [Fact]
-        public void ValidEnum()
+        public void Enum_Check_Valid()
         {
             Assert.True(EnumHelpers.IsValidName<TestEnums>("EnumA"));
         }
 
         [Fact]
-        public void InvalidEnum()
+        public void Enum_Check_Invalid()
         {
             Assert.False(EnumHelpers.IsValidName<TestEnums>("EnumC"));
         }
 
         [Fact]
-        public void ValidGetEnum()
+        public void Enum_Get_Valid()
         {
             var enumA = EnumHelpers.GetEnum<TestEnums>("EnumA");
 
             Assert.Equal(TestEnums.EnumA, enumA);
+        }
+
+        [Fact]
+        public void Enum_Get_Invalid_Exception()
+        {
+            Assert.Throws<InvalidEnumException>(delegate { EnumHelpers.GetEnum<TestEnums>("EnumC"); });
         }
     }
 
