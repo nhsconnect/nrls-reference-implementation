b1f4a8e5a4569bbd441a460db3d96b6dadb8b25f
Martin Wilkins <martin.wilkins@answerdigital.com>
2018-04-24 09:50:53 +0100

    uri helper tests

--- Demonstrator/Demonstrator.Utilities/Extensions/UriExt.cs
+++ Demonstrator/Demonstrator.Utilities/Extensions/UriExt.cs
@@ -10,17 +10,17 @@ namespace Demonstrator.Utilities.Extensions
 
             if (!string.IsNullOrEmpty(query))
             {
-                var queryParameters = query.Substring(1).Split("&");
+                var queryParameters = (query.StartsWith("?") ? query.Substring(1) : query).Split("&");
 
                 foreach(var qp in queryParameters)
                 {
                     var kvp = qp.Split("=");
 
-                    if(kvp.Length == 2)
+                    if(kvp.Length == 2 && !string.IsNullOrWhiteSpace(kvp[0]))
                     {
                         parameters.Add(kvp[0], kvp[1]);
                     }
-                    else if (kvp.Length == 1)
+                    else if (kvp.Length == 1 && !string.IsNullOrWhiteSpace(kvp[0]))
                     {
                         parameters.Add(kvp[0], null);
                     }
---
+++ Demonstrator/DemonstratorTest.Utilities/UriExtTests.cs
@@ -0,0 +1,91 @@
+﻿using Demonstrator.Utilities.Extensions;
+using Xunit;
+
+namespace DemonstratorTest.Utilities
+{
+    public class UriExtTests
+    {
+        [Fact]
+        public void Uri_From_QueryString_Valid_AllEmpty()
+        {
+            var test = "";
+
+            var parsedUri = test.ParseQuery();
+
+            Assert.Equal(0, parsedUri.Count);
+
+        }
+
+        [Fact]
+        public void Uri_From_QueryString_Valid_OneEmpty()
+        {
+            var test = "test";
+
+            var parsedUri = test.ParseQuery();
+
+            Assert.True(parsedUri.ContainsKey("test"));
+            Assert.Null(parsedUri["test"]);
+
+        }
+
+        [Fact]
+        public void Uri_From_QueryString_Valid_OneComplete()
+        {
+            var test = "test=true";
+
+            var parsedUri = test.ParseQuery();
+
+            Assert.True(parsedUri.ContainsKey("test"));
+            Assert.Equal("true", parsedUri["test"]);
+        }
+
+        [Fact]
+        public void Uri_From_QueryString_Valid_OneComplete_OneEmpty()
+        {
+            var test = "test=false&anothertest";
+
+            var parsedUri = test.ParseQuery();
+
+            Assert.True(parsedUri.ContainsKey("test"));
+            Assert.Equal("false", parsedUri["test"]);
+
+            Assert.True(parsedUri.ContainsKey("anothertest"));
+            Assert.Null(parsedUri["anothertest"]);
+        }
+
+        [Fact]
+        public void Uri_From_QueryString_Valid_AllEmpty_OnePartial()
+        {
+            var test = "&another";
+
+            var parsedUri = test.ParseQuery();
+
+            Assert.True(parsedUri.ContainsKey("another"));
+            Assert.Null(parsedUri["another"]);
+        }
+
+        [Fact]
+        public void Uri_From_QueryString_Valid_TwoComplete()
+        {
+            var test = "test=true&anothertest=false";
+
+            var parsedUri = test.ParseQuery();
+
+            Assert.True(parsedUri.ContainsKey("test"));
+            Assert.Equal("true", parsedUri["test"]);
+
+            Assert.True(parsedUri.ContainsKey("anothertest"));
+            Assert.Equal("false", parsedUri["anothertest"]);
+        }
+
+        [Fact]
+        public void Uri_From_QueryString_Valid_OnePartial()
+        {
+            var test = "&=false";
+
+            var parsedUri = test.ParseQuery();
+
+            Assert.Equal(0, parsedUri.Count);
+        }
+    }
+}
