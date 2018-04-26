c1b36865b9d36b0bf842b4519df3f9cafb5dd864
Martin Wilkins <martin.wilkins@answerdigital.com>
2018-04-26 11:06:25 +0100

    new pointer service tests

---
+++ Demonstrator/DemonstratorTest.Services/Nrls/PointerServiceTests.cs
@@ -0,0 +1,106 @@
+﻿using Demonstrator.Core.Interfaces.Services.Fhir;
+using Demonstrator.Models.Nrls;
+using Demonstrator.NRLSAdapter.Helpers;
+using DemonstratorTest.Data.Helpers;
+using Hl7.Fhir.Model;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SystemTasks = System.Threading.Tasks;
+using Xunit;
+using Demonstrator.Services.Service.Nrls;
+using Demonstrator.Models.ViewModels.Base;
+using DemonstratorTest.Comparer;
+using Demonstrator.Models.ViewModels.Nrls;
+
+namespace DemonstratorTest.Services.Nrls
+{
+    public class PointerServiceTests
+    {
+        [Fact]
+        public async void PointerService_Returns_ValidPointerList()
+        {
+            var patientModels = MongoPatients.Patients.ToList();
+            var organizationModels = MongoOrganizations.Organizations.ToList();
+            var documentReferenceModels = MongoDocumentReferences.DocumentReferences_3656987882;
+
+             var testNhsNumber = "3656987882";
+            var testAsid = "200000000116";
+            var testOrgCode = "AMS01";
+
+            var _docRefService = new Mock<IDocumentReferenceServices>();
+            _docRefService.Setup(m => m.GetPointersAsBundle(It.IsAny<NrlsPointerRequest>())).Returns(SystemTasks.Task.Run(() => GetBundle<DocumentReference>(documentReferenceModels))).Verifiable();
+
+            var _patientService = new Mock<IPatientServices>();
+            _patientService.Setup(m => m.GetPatients()).Returns(SystemTasks.Task.Run(() => patientModels)).Verifiable();
+
+            var _organisationServices = new Mock<IOrganisationServices>();
+            _organisationServices.Setup(m => m.GetOrganisations()).Returns(SystemTasks.Task.Run(() => organizationModels)).Verifiable();
+
+            var pointerService = new PointerService(_docRefService.Object, _patientService.Object, _organisationServices.Object);
+
+            var request = RequestViewModel.Create(testNhsNumber);
+            request.Asid = testAsid;
+            request.OrgCode = testOrgCode;
+
+            var actual = await pointerService.GetPointers(request);
+
+            var pointers = MongoPointerViewModels.PointerViewModels_3656987882;
+            var patient = MongoPatientViewModels.PatientViewModel_3656987882;
+            var org = MongoOrganizationViewModels.OrganizationViewModel_00003X;
+
+            var expected = new List<PointerViewModel>();
+
+            foreach (var exp in pointers)
+            {
+                exp.SubjectViewModel = patient;
+                exp.AuthorViewModel = org;
+                exp.CustodianViewModel = org;
+                expected.Add(exp);
+            }
+
+            _docRefService.Verify();
+            _patientService.Verify();
+            _organisationServices.Verify();
+
+            Assert.Equal(expected, actual, Comparers.ModelComparer<PointerViewModel>());
+
+        }
+
+        private Bundle GetBundle<T>(IList<T> resources) where T : Resource
+        {
+            var bundle = new Bundle
+            {
+                Id = Guid.NewGuid().ToString(),
+                Meta = new Meta
+                {
+                    LastUpdated = DateTime.UtcNow,
+                    VersionId = Guid.NewGuid().ToString()
+                },
+                Type = Bundle.BundleType.Searchset,
+                Total = resources.Count,
+                Link = new List<Bundle.LinkComponent>
+                {
+                    new Bundle.LinkComponent
+                    {
+                        Relation = "_self",
+                        Url = "TestLink"
+                    }
+                },
+                Entry = resources.Select(r => new Bundle.EntryComponent
+                {
+                    Search = new Bundle.SearchComponent
+                    {
+                        Mode = Bundle.SearchEntryMode.Match
+                    },
+                    FullUrl = $"ResourceLink",
+                    Resource = r
+                }).ToList()
+
+            };
+
+            return bundle;
+        }
+    }
+}
