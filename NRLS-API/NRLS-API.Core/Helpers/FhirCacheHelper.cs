9512a77004c0b9cea75bc8d2673badddf9fcb0fa
Martin Wilkins <martin.wilkins@answerdigital.com>
2018-06-30 09:33:34 +0100

    move resource resolver for shared access

---
+++ NRLS-API/NRLS-API.Core/Helpers/FhirCacheHelper.cs
@@ -0,0 +1,33 @@
+﻿using Hl7.Fhir.Rest;
+using Hl7.Fhir.Specification.Source;
+using System.IO;
+
+namespace NRLS_API.Core.Helpers
+{
+    public static class FhirCacheHelper
+    {
+        private static IResourceResolver _source { get; set; }
+
+        public static IResourceResolver Source
+        {
+            get
+            {
+                if(_source == null)
+                {
+                    _source = GetResolver();
+                }
+
+                return _source;
+            }
+        }
+
+        private static IResourceResolver GetResolver()
+        {
+            var basePath = DirectoryHelper.GetBaseDirectory();
+
+            var zip = Path.Combine(basePath, "Data\\definitions.xml.zip");
+
+            return new CachedResolver(new MultiResolver(new WebResolver(uri => new FhirClient("https://fhir.nhs.uk/STU3")), new ZipSource(zip)));
+        }
+    }
+}
