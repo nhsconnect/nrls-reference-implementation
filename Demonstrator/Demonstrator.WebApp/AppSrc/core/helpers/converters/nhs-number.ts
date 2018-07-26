3c5e21648bb72d59c11f1cd65364488497678bd1
Martin Wilkins <junior_bilko@hotmail.com>
2018-07-26 15:45:38 +0100

    bug on nhs number converter

--- Demonstrator/Demonstrator.WebApp/AppSrc/core/helpers/converters/nhs-number.ts
+++ Demonstrator/Demonstrator.WebApp/AppSrc/core/helpers/converters/nhs-number.ts
@@ -2,6 +2,6 @@
 
 export class NhsNumberValueConverter {
     toView(value) {
-        return !value || value.length !== 9 ? value : `${value.substr(0, 3)} ${value.substr(3, 3)} ${value.substr(6, 3)}`;
+        return !value || value.length !== 10 ? value : `${value.substr(0, 3)} ${value.substr(3, 3)} ${value.substr(6, 4)}`;
     }
 }
