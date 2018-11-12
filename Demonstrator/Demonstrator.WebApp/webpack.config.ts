365f71dd3cf1bc1eb308da55024e6d462e104963
Martin Wilkins <martin.wilkins@answerdigital.com>
2018-11-12 14:34:07 +0000

    update version

--- Demonstrator/Demonstrator.WebApp/webpack.config.ts
+++ Demonstrator/Demonstrator.WebApp/webpack.config.ts
@@ -32,7 +32,7 @@ const ENV: string = process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() |
 
 // basic configuration:
 const title = 'NRLS Demonstrator';
-const appVersion = '0.1.5';
+const appVersion = '0.1.6';
 const gaTrackId = 'UA-38028819-4';
 const cookieBotId = '30398f35-a144-4edd-a779-05481f9da7b3';
 const baseUrl = '/';
