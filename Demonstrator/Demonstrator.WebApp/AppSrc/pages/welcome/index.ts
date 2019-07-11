a7eed84f053f19a7c0d24fd3e470310d1f388397
Martin Wilkins <martin.wilkins@answerdigital.com>
2019-07-11 20:26:49 +0100

    change demo app home page to redirect to NHS digital website

--- Demonstrator/Demonstrator.WebApp/AppSrc/pages/welcome/index.ts
+++ Demonstrator/Demonstrator.WebApp/AppSrc/pages/welcome/index.ts
@@ -1,23 +1,5 @@
-import { IActorOrganisation }   from "../../core/interfaces/IActorOrganisation";
-import { ActorOrganisationSvc } from "../../core/services/ActorOrganisationService";
-import { bindable, inject }     from "aurelia-framework";
-
-@inject(ActorOrganisationSvc)
 export class Welcome {
     heading: string = 'Welcome to the NRLS Interactive Guide';
-    actorOrganisations: Array<IActorOrganisation>;
-    orgsLoading: boolean = false;
-
-    constructor(private actorOrgSvc: ActorOrganisationSvc) { }
-
-    created() {
-        this.orgsLoading = true;
-        this.actorOrgSvc.getAll().then(actorOrgs => {
-            this.actorOrganisations = actorOrgs;
-            this.orgsLoading = false;
-        });
-    }
-
 }
 
 
